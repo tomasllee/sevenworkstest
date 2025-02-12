ddd8cd0c7a061e1aa8482d0f804e71acf01c2984
alexlautin <alexalautin@gmail.com>
2025-02-12 17:07:26 -0500

    changes

--- sevenworks/src/app/editor/page.tsx
+++ sevenworks/src/app/editor/page.tsx
@@ -1,10 +1,9 @@
 export default function Page() {
     return (
         <div className="flex flex-col gap-4 p-4">
-            <h1 className="text-2xl font-bold text-black">Editor</h1>
-            <p className="text-gray-700">
-                This is a basic text editor built with Next.js and Tailwind CSS. It
-                supports basic text formatting, lists, and links.
+            <h1 className="text-2xl font-bold text-white">Text Editor</h1>
+            <p className="text-black-700">
+                Editor
             </p>
         </div>
     );
